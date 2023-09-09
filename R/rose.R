e37f43f89483664f2589977ff891ccfeea78a9ea
Ronan SYMONEAUX <54936128+ronysym@users.noreply.github.com>
2023-09-09 18:29:54 +0200

    Suppression data HRATA apple et remplacer par data Rose

---
+++ R/rose.R
@@ -0,0 +1,20 @@
+#' HRATA data on rose flower
+#'
+#' Data from a tasting done on 9 rose cultivars with 74 tasters in 2023
+#'
+#'
+#' @docType data
+#'
+#' @usage data(rose)
+#'
+#' @format a dataset with 9 rose bushes and 74 tasters
+#' \describe{
+#'   \item{Juge}{74 tasters recruited by ESA Angers - UR GRAPPE}
+#'   \item{Product}{9 rose cultivars}
+#'   \item{Attributes}{118 sensory attributes scored from NA to 3 following the HRATA methodology}
+#' }
+#'
+#' @keywords datasets
+#'
+#'
+"rose"
---
+++ R/rose.attribute.R
@@ -0,0 +1,23 @@
+#' Hierarchical structure of the attributes in HRATA rose dataset
+#'
+#' Dataframe of the rose flower aromatic wheel structured with three columns and presenting the hierarchical organization of the dataset (wheel)
+#' Family and Category are duplicated to express the hierarchical structure
+#'
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage data(rose.attribute)
+#'
+#' @format a dataframe with 3 columns (Family,Category and Attribute)
+#' \describe{
+#'   \item{Family}{Aromatic families}
+#'   \item{Category}{Categories under the families}
+#'   \item{Attribute}{Attributes or terms under category and family}
+#' }
+#'
+#' @keywords datasets
+#'
+#'
+"rose.attribute"
