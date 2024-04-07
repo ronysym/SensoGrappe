a9e38d44d86655c31c852c930d62d9d6e4c37dd6
Ronan SYMONEAUX <54936128+ronysym@users.noreply.github.com>
2024-04-07 17:15:54 +0200

    Ajout Message d'erreur a la fonction en fonction des paramètres
    What
    -Ajout de checks au debut de la fonction avec erreur si le paramètre n'est pas bon

--- R/Var.Grappe.R
+++ R/Var.Grappe.R
@@ -1,12 +1,12 @@
 #' Var.Grappe : Checking and encoding dataset
 #'
-#' @description Check dataset and  encode easily the vector (variables) to factor, integer or others
+#' @description Check dataset and  encode easily the vector (variables) to factor, integer or numeric
 #'
 #' @param x 	dataframe
 #' @param column the numbers of the columns to change the type
-#' @param type expected type : "factor", "numeric" or "integer"
-#' @param filter text for searching name in column (letter or number)
-#' @param verbose  Display of the results ? TRUE by default
+#' @param type expected type of the columns to be changed : "factor", "numeric" or "integer". NULL by default
+#' @param filter character or numeric corresponding to searching name or number of column
+#' @param verbose  logical display of the results. TRUE by default
 #'
 #' @examples
 #' data(wine)
@@ -16,13 +16,39 @@
 #'
 #' @export
 Var.Grappe <- function(x, column = 0, type = NULL, filter = NULL, verbose = TRUE) { # Cree la matrice de resultats
+
+  # Check if x is a dataframe
+  if (!is.data.frame(x)) {
+    stop("Argument 'x' must be a dataframe.")
+  }
+
+  # Check if column is numeric
+  if (!is.numeric(column)) {
+    stop("Argument 'column' must be numeric.")
+  }
+
+  # Check if type is one of "factor", "numeric", or "integer"
+  if (!is.null(type) && !type %in% c("factor", "numeric", "integer")) {
+    stop("Argument 'type' must be one of 'factor', 'numeric', or 'integer'.")
+  }
+
+  # Check if verbose is logical
+  if (!is.logical(verbose)) {
+    stop("Argument 'verbose' must be logical.")
+  }
+
+  # Check if filter is character or numeric
+  if (!is.null(filter) && !is.character(filter) && !is.numeric(filter)) {
+    stop("Argument 'filter' must be either character or numeric.")
+  }
+
   allvar <- matrix(1:length(names(x)), ncol = 2, nrow = length(names(x)), byrow = FALSE)
   colnames(allvar) <- c("Column", "Type of variable")
   rownames(allvar) <- names(x)
   # Liste des types de variables possibles pour les transformations
   fac <- c("f", "factor")
   num <- c("n", "numeric")
-  int <- c("i", "integes")
+  int <- c("i", "integer")
   #
   if (column[1] != 0 && !is.null(type)) {
     for (i in 1:length(column))
@@ -51,7 +77,7 @@ Var.Grappe <- function(x, column = 0, type = NULL, filter = NULL, verbose = TRUE
     } else {
       ligne.temp <- grep(filter, rownames(allvar), ignore.case = TRUE)
       if (length(ligne.temp) == 0) {
-        cat("\nAucune variable du tableau ne porte ce nom.\nVerifiez l'orthographe\n\n")
+        cat("\nNo variable in the dataframe has this name.Check the spelling\n\n")
       } else {
         print(as.data.frame(allvar)[ligne.temp, ], right = FALSE)
       }
